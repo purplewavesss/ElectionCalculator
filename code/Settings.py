71012e2fa317a9943cf84d970da81e064f90fed9
Gavin Grotegut <gavin.grotegut@gmail.com>
2023-02-07 15:10:36 -0700

    SettingsDialog now modifies Settings

--- code/Settings.py
+++ code/Settings.py
@@ -4,6 +4,6 @@ from Methods import Methods
 class Settings:
     def __init__(self):
         self.method_type = Methods.HIGHEST_AVERAGE_METHOD.value
-        self.current_methods: dict[str, str] = {"highest_avg": "d_hondt", "largest_remainder": "hare"}
+        self.current_methods: dict[str, str] = {"highest_average": "d_hondt", "largest_remainder": "hare"}
         self.votes_forced = False
         self.forced_vote_num: int = 0
--- code/SettingsDialog.py
+++ code/SettingsDialog.py
@@ -8,3 +8,36 @@ class SettingsDialog(UiSettingsDialog, QtWidgets.QDialog):
         super(SettingsDialog, self).__init__()
         self.setup_ui(self)
         self.settings: Settings = _settings
+        self.implement_triggers()
+        if self.settings.votes_forced:
+            self.vote_number.setText(str(self.settings.forced_vote_num))
+
+    def implement_triggers(self):
+        self.highest_average_box.toggled.connect(self.ham_trigger)
+        self.largest_remainder_box.toggled.connect(self.lrm_trigger)
+        self.button_box.accepted.connect(self.change_settings)
+        self.button_box.rejected.connect(self.reject)
+        self.vote_number.editingFinished.connect(lambda: self.button_box.button(
+                                                 QtWidgets.QDialogButtonBox.StandardButton.Ok).setEnabled(True))
+
+    def ham_trigger(self):
+        self.largest_remainder_box.setChecked(not self.highest_average_box.isChecked())
+        self.button_box.button(QtWidgets.QDialogButtonBox.StandardButton.Ok).setEnabled(True)
+
+    def lrm_trigger(self):
+        self.highest_average_box.setChecked(not self.largest_remainder_box.isChecked())
+        self.button_box.button(QtWidgets.QDialogButtonBox.StandardButton.Ok).setEnabled(True)
+
+    def change_settings(self):
+        for group_box in [self.election_method_box, self.highest_average_box]:
+            for radio_button in group_box.children():
+                radio_button: QtWidgets.QRadioButton
+                if radio_button.isChecked():
+                    self.settings.current_methods[group_box.objectName()] = radio_button.objectName()
+        self.settings.method_type = self.highest_average_box.isChecked()
+
+        if self.vote_number.text != "":
+            self.settings.votes_forced = True
+            self.settings.forced_vote_num = int(self.vote_number.text())
+
+        self.accept()
